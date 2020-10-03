ff817298232f68ac32e682e7698005c1be281349
mm@tdsgn.ru <mm@tdsgn.ru>
2020-10-03 18:29:38 +0700

    добавил prettier.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,80 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-const fs = require('fs');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const fs = require("fs");
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 function generateHtmlPlugins(templateDir) {
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  
-  return templateFiles.map(item => {
-    const parts = item.split('.'),
+
+  return templateFiles.map((item) => {
+    const parts = item.split("."),
       name = parts[0],
       extension = parts[1];
-    
-      return new HtmlWebpackPlugin({
-        filename: `../${name}.html`,
-        template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-        inject: false,
-      })
-  })
+
+    return new HtmlWebpackPlugin({
+      filename: `../${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: false,
+    });
+  });
 }
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  entry: path.resolve(__dirname, "src", "js", "index.js"),
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist', 'js')
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist", "js"),
   },
   plugins: [
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(NODE_ENV)
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development",
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'templates', 'index.html'),
-      filename: '../index.html'
+      template: path.resolve(__dirname, "src", "templates", "index.html"),
+      filename: "../index.html",
+      inject: false,
     }),
-  ].concat(generateHtmlPlugins(path.resolve(__dirname, 'src', 'templates'))),
+  ].concat(generateHtmlPlugins(path.resolve(__dirname, "src", "templates"))),
   module: {
     rules: [
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
-        ]
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
           },
         ],
       },
-    ]
+    ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, "dist"),
     compress: true,
     noInfo: true,
     overlay: true,
-    port: 9000
+    port: 9000,
   },
-  devtool: NODE_ENV === 'development' ?  'eval-cheap-source-map' : false
+  devtool: NODE_ENV === "development" ? "eval-cheap-source-map" : false,
 };
- 
